ffba9c4fc98d1aa2c6198154f4a5d7bf9a795ed7
ViktorSolomianiy <xmegasystem35@gmail.com>
2022-12-22 01:08:50 +0200

    123

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,10 +1,20 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = 'key=32253350-233c165c5f822b5f85a9694b5';
 
-export default function fetchImages(inputValue) {
-  fetch(
-    `${BASE_URL}?${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(resp => {
-    return resp.json();
-  });
+export default function fetchImages(value) {
+  axios
+    .get(
+      `${BASE_URL}?${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+    .then(resp => {
+      return resp.data;
+    });
+
+  //   fetch(
+  //     `${BASE_URL}?${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
+  //   ).then(resp => {
+  //     return resp.json();
+  //   });
 }
