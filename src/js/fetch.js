80fa416fae51a546c9292b46c1e50e78a916c51e
ViktorSolomianiy <xmegasystem35@gmail.com>
2022-12-22 00:19:27 +0200

    1

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,9 +1,9 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = 'key=32253350-233c165c5f822b5f85a9694b5';
 
-export default function fetchImages(value) {
+export default function fetchImages(inputValue) {
   fetch(
-    `${BASE_URL}?${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`
+    `${BASE_URL}?${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
   ).then(resp => {
     return resp.json();
   });
