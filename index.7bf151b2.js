10fc4495624299de65ee845e0cc7b860505e3e06
ViktorSolomianiy <108832355+ViktorSolomianiy@users.noreply.github.com>
2022-12-21 21:35:09 +0000

    Deploying to gh-pages from @ ViktorSolomianiy/goit-js-hw-11@8b8ca9f091d74d9a64c199ddb6a7e4738511b1df 🚀

---
+++ index.7bf151b2.js
@@ -0,0 +1,2 @@
+fetch("https://pixabay.com/api/?key=32253350-233c165c5f822b5f85a9694b5&q=yellow+flowers&image_type=photo").then((function(o){console.log(o.json())}));
+//# sourceMappingURL=index.7bf151b2.js.map
---
+++ index.f6c5ab67.js
@@ -0,0 +1,2 @@
+fetch("https://pixabay.com/api/?key=32253350-233c165c5f822b5f85a9694b5&q=yellow+flowers&image_type=photo").then((o=>{console.log(o.json())}));
+//# sourceMappingURL=index.f6c5ab67.js.map
