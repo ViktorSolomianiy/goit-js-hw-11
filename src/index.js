6090513ace331dbc827e3d78bfc94808286bbf81
ViktorSolomianiy <xmegasystem35@gmail.com>
2022-12-23 07:38:54 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -5,13 +5,48 @@ import simpleLightbox from 'simplelightbox';
 const formEl = document.querySelector('.search-form');
 const inputEl = document.querySelector('input');
 const btnEl = document.querySelector('button');
+const gallery = document.querySelector('.gallery');
 
 formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
 
-  const inputValue = e.target.value;
+  const inputValue = inputEl.value.trim();
 
-  fetchImages(inputValue);
+  fetchImages(inputValue)
+    .then(createPhotoList)
+    .catch(err => console.log(err));
+}
+
+function createPhotoList(photos) {
+  const photosArray = photos.hits;
+
+  const markupPhotoList = photosArray
+    .map(
+      photo =>
+        `
+          <div class="photo-card">
+           <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy"/>
+           <div class="info">
+               <p class="info-item">
+                <b>Likes</b><br />${photo.likes}
+              </p>
+              <p class="info-item">
+                <b>Views</b><br />${photo.views}
+              </p>
+              <p class="info-item">
+                <b>Comments</b><br />${photo.comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads</b><br />${photo.downloads}
+              </p>
+           </div>
+          </div>
+         `
+    )
+    .join();
+
+  gallery.innerHTML = markupPhotoList;
+  // console.log(markupPhotoList);
 }
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -4,12 +4,13 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = 'key=32253350-233c165c5f822b5f85a9694b5';
 
 export default function fetchImages(value) {
-  axios
+  return axios
     .get(
       `${BASE_URL}?${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`
     )
     .then(resp => {
       return resp.data;
+      // console.log(resp.data.hits);
     });
 
   //   fetch(
